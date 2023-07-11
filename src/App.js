57392783f3f6339357ec14f8a178918faaac479f
drv98 <drv98@naver.com>
2023-07-11 13:58:48 +0900

    홈컴포넌트 추가 테이블 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Navbar from "./layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
