4d2f5f4c11cc26d24ccc7a91e37a4811dcd8df41
drv98 <drv98@naver.com>
2023-07-11 15:34:23 +0900

    라우터 라이브러리 설치 가입하기 화면

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
