422e6603626b89696347b71868a48febc8526ded
drv98 <drv98@naver.com>
2023-07-11 12:07:29 +0900

    부트스트랩 라이브러리 추가 네브바 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Navbar from "./layout/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
